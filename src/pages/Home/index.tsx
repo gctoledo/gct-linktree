dda461d9d6a596f6a5fc8e03991629c42632e27f
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-23 20:16:13 -0300

    feat: add Home page with Social componenent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
+import Social from "../../components/Social";
+
+import { FaGithub, FaInstagram } from "react-icons/fa";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className=" md:text-4xl text-3xl font-bold text-white mt-20">
+        Gabriel Costa Toledo
+      </h1>
+
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform duration-200 hover:scale-105 cursor-pointer">
+          <a href="#">
+            <p className="text-base md:text-lg">Github</p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url={"https://github.com/gctoledo"}>
+            <FaGithub size={35} color="#fff" />
+          </Social>
+
+          <Social url={"https://instagram.com/gctoledo_"}>
+            <FaInstagram size={35} color="#fff" />
+          </Social>
+        </footer>
+      </main>
     </div>
   );
 };
